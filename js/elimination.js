1d7eac0085217c9f43e883c171398c021922b125
Cook393 <cook393@morrisville.edu>
2021-11-13 16:48:40 -0500

    Elimination page code clean-up

--- js/elimination.js
+++ js/elimination.js
@@ -1,103 +1,117 @@
-var counter = 0;
-var ul = document.getElementById("results");
-const button = document.getElementById("submit");
+//DATA
 
-$(function(){$("#results").sortable({connectWith:"#trash, #keep"})});
-
-$(function(){$("#trash").sortable()});
-
-$(function(){$("#keep").sortable()});
+	var counter = 0;
+	var ul = document.getElementById("results");
+	const button = document.getElementById("submit");
 
 
-//needs work, requests should be validated
+//EVENT LISTENERS
 
-window.onload = function(){
-
-	var request = new XMLHttpRequest();
-	request.open("GET", "../json/results.json");
-
-	request.onload = function(){
-
-		var data = JSON.parse(request.responseText);
-		displayRestaurant(data);
-	};
-
-	request.send();
-}
-
-$("#results").on("sortremove", function(event, ui){
-
-	var request = new XMLHttpRequest();
-	request.open("GET", "../json/results.json");
-
-	request.onload = function(){
-
-		var data = JSON.parse(request.responseText);
-		displayRestaurant(data);
-	};
-
-	request.send();
-});
-
-function displayRestaurant(data){
-
-	var length = data.length;
-			
-	if(counter < length){
-
-  		htmlString = 
-
-	  		`
-	  		<li class="ui-state-default" id=\"${counter}\">
-	  		<table border="1" bgcolor="white">
-			<tr><td rowspan=\"3\"><img src=\" ${data[counter].image} \"></img></td>
-			<td><h2> ${data[counter].RestName} </h2></td>
-			<td><b> Rating:</b> ${data[counter].Rating} Stars</td</tr>
-			<tr>
-			<td><b> Category:</b> ${data[counter].Category} </td>
-			<td><b> Average Dish Price:</b> ${data[counter].PricePoint} </td></tr>
-			<tr>
-			<td><b> Address:</b> ${data[counter].Address} ${data[counter].City} ${data[counter].State} ${data[counter].ZipCode} </td>
-			<td><a href= \"${data[counter].URL}\">Website Link</a><br><b> Phone Number:</b> ${data[counter].PhoneNumber} </td>
-			</tr>
-			</table>
-	  		`
-
-  		ul.innerHTML = htmlString;
-  		counter++;
-	}
-	else{ displayFinalResults(); }
-}
-
-
-//needs work
-function displayFinalResults(){
-
-	var topPicks = document.getElementById("right");
-	topPicks.className = "topPicks";
+	//on load function
+	window.onload = function(){ ProcessData(); }
 	
-	$("#trash").remove();
-	$("#results").remove();
-	$(".left").remove();
-	$(".center").remove();
+	//SORTABLE LISTS
+
+		// ---> JQuery Sortables API Documentation: https://api.jqueryui.com/sortable/
+
+		$(function(){$("#results").sortable({connectWith:"#trash, #keep"})});
+
+		$(function(){$("#trash").sortable()});
+
+		$(function(){$("#keep").sortable()});
+
+	//li removal listener
+	$("#results").on("sortremove", function(event, ui){ ProcessData(); });
 
 
-	var list = $("#keep")
+//AJAX REQUEST
 
-	if(list.length > 0){ $("<h2>Top Choices</h2>").insertBefore(list); }
+	// ---> AJAX tutorial: https://www.youtube.com/watch?v=rJesac0_Ftw&t=1591s&ab_channel=LearnWebCode
 
-	const randomClone = cloneRandomLi();
+	//creates an ajax request and displays the current response
+	function processData(){
 
-	$(randomClone).insertBefore(topPicks);
-	$("<h2>Our Pick</h2>").insertBefore(randomClone);
-}
+		var request = new XMLHttpRequest();
+		request.open("GET", "../json/results.json");
+
+		request.onload = function(){
+
+			var data = JSON.parse(request.responseText);
+			displayRestaurant(data);
+		};
+
+		request.send();
+	}
 
 
-//getting random li needs work
-function cloneRandomLi(){
+//DYNAMIIC HTML
 
-	var clone = $("#keep li:nth-child(1)");
-  	clone.className = "ourPick";
+	//recieves ajax request data and adds the current restaurant to the page, or displays the final results
+	function displayRestaurant(data){
 
-  	return clone;
-}
+		//current results.json data
+		var length = data.length;
+		
+		//loop through json array
+		if(counter < length){
+
+			//create restaurant table in HTML
+	  		htmlString = 
+
+	  			//template literal
+
+		  		`
+		  		<li class="ui-state-default" id=\"${counter}\">
+		  		<table border="1" bgcolor="white">
+				<tr><td rowspan=\"3\"><img src=\" ${data[counter].image} \"></img></td>
+				<td><h2> ${data[counter].RestName} </h2></td>
+				<td><b> Rating:</b> ${data[counter].Rating} Stars</td</tr>
+				<tr>
+				<td><b> Category:</b> ${data[counter].Category} </td>
+				<td><b> Average Dish Price:</b> ${data[counter].PricePoint} </td></tr>
+				<tr>
+				<td><b> Address:</b> ${data[counter].Address} ${data[counter].City} ${data[counter].State} ${data[counter].ZipCode} </td>
+				<td><a href= \"${data[counter].URL}\">Website Link</a><br><b> Phone Number:</b> ${data[counter].PhoneNumber} </td>
+				</tr>
+				</table>
+		  		`
+
+		  	//change the inner HTML of results List
+	  		ul.innerHTML = htmlString;
+	  		counter++;
+		}
+		else{ displayFinalResults(); }
+	}
+
+
+	//final results display
+	function displayFinalResults(){
+
+		var topPicks = document.getElementById("right");
+		topPicks.className = "topPicks";
+		
+		$("#trash").remove();
+		$("#results").remove();
+		$(".left").remove();
+		$(".center").remove();
+
+		var list = $("#keep")
+
+		if(list.length > 0){ $("<h2>Top Choices</h2>").insertBefore(list); }
+
+		const randomClone = cloneRandomLi();
+
+		$(randomClone).insertBefore(topPicks);
+		$("<h2>Our Pick</h2>").insertBefore(randomClone);
+	}
+
+
+	
+	
+	//clone random list item
+	function cloneRandomLi(){
+
+		var clone = $("#keep li:nth-child(1)");
+
+	  	return clone;
+	}
